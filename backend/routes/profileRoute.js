2a633941924e0a686eec11f19d936ee4d17c5e77
I-del-hub <ishmarikashah496@gmail.com>
2022-05-19 15:20:32 +0545

    User profile view by Admin

--- backend/routes/profileRoute.js
+++ backend/routes/profileRoute.js
@@ -5,7 +5,7 @@ const router = new express.Router();
 const upload= require("../uploads/uploads");
 const auth= require("../auth/authentication");
 
-//get single view
+
 router.get("/profile/single/:uid", function(req,res){
     const uid = req.params.uid
     
@@ -20,61 +20,60 @@ router.get("/profile/single/:uid", function(req,res){
 })
 
 
-//hotel booking for users'
+
 router.put("/profile/insert/:pid",upload.single('profile_image'), function (req, res) {
     
-  console.log(req.body)
-  // const id=req.adminInfo.id;
-  const fullname =req.body.fullname;
-  const phone =req.body.phone;
-  const gender =req.body.gender;
-  const dateofbirth =req.body.dateofbirth;
-  const address =req.body.address;
-  const photo =req?.file?.filename;
-  const profile = User.findOneAndUpdate(
-      {_id:req.params.pid},
+    console.log(req.body)
+   
+    const fullname =req.body.fullname;
+    const phone =req.body.phone;
+    const gender =req.body.gender;
+    const dateofbirth =req.body.dateofbirth;
+    const address =req.body.address;
+    const photo =req?.file?.filename;
+    const profile = User.findOneAndUpdate(
+        {_id:req.params.pid},
+        
+        {
       
-      {
-      // id:id,
-      fullname:fullname,
-      phone:phone,
-      gender:gender,
-      dateofbirth:dateofbirth,
-      address:address,
-      photo:photo,
-  })
-  .then(function (e) {
-      console.log(e);
-       res.json({mes:"data is inserted", success:true}) 
-       }).catch(function (e) {
-           console.log(e);
-          res.json({
-              mes:"something went wrong!!"
-          })
-   })
+        fullname:fullname,
+        phone:phone,
+        gender:gender,
+        dateofbirth:dateofbirth,
+        address:address,
+        photo:photo,
+    })
+    .then(function (e) {
+        console.log(e);
+         res.json({mes:"data is inserted", success:true}) 
+         }).catch(function (e) {
+             console.log(e);
+            res.json({
+                mes:"something went wrong!!"
+            })
+     })
 })
 
 
-router.get("/profile/details", function (req, res) {
-  const fullname =req.body.fullname;
-  const phone =req.body.phone;
-  const gender =req.body.gender;
-  const dateofbirth =req.body.dateofbirth;
-  const address =req.body.address;
-  const photo =req?.file?.filename;
 
-  User.find(fullname,address,phone,gender,dateofbirth,photo)
-    .then(function (result) {
-      res.json(result);
-      console.log(result);
+
+  router.put("/profile/approve",  function (req, res) {
+    const id = req.body.id
+    User.findOne({ _id: id })
+        .then(function (result) {
+                    if(result.isApproved){
+                        User.updateOne({ _id: id }, { isApproved: false })
+                    .then(function () {
+                        res.json({ msg: "Visitor kyc Approved Successfully", success: true })
+                    })
+                    }
+                    else{
+                        User.updateOne({ _id: id }, { isApproved: true })
+                    .then(function () {
+                        res.json({ msg: "Visitor kyc Approved Successfully", success: true })
+                    })
+                    }
     })
-
-    .catch(function () {
-      res.json({ msg: "something went wrong" });
-    });
-});
-
-
-
+  })
 
 module.exports=router
