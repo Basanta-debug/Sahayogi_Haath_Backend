cc84cca8cc626ba9893eaddb82afdd4388e621b1
cju100 <shreeju.joshi000@gmail.com>
2022-07-04 17:28:14 +0545

    Like in comment Backend

---
+++ backend/models/commentModel.js
@@ -0,0 +1,34 @@
+// const { builtinModules } = require("module");
+const mongoose = require("mongoose");
+// const { required } = require("nodemon/lib/config");
+//customer model
+const Comment = mongoose.model('Comment',{
+    
+   text:{
+       type:String
+   },
+
+   user:{
+    type: mongoose.Schema.Types.ObjectId, ref: "User"
+   },
+   cook:{
+    type: mongoose.Schema.Types.ObjectId, ref: "Cook"
+   },
+   
+   rating:{
+    type:Number,
+
+   },
+
+   like:{
+    type:Array
+   },
+
+   flag:{
+    type:Array
+   }
+
+
+})
+
+module.exports = Comment;
