5ba87b8f4eb722ee11193d2ed2247665e42defc4
cju100 <shreeju.joshi000@gmail.com>
2022-07-11 08:44:51 +0545

    Like in comment backend

--- backend/routes/commentRoute.js
+++ backend/routes/commentRoute.js
@@ -3,42 +3,37 @@ const express = require("express");
 const Comment = require("../models/commentModel");
 const router = new express.Router();
 
-router.post("/comment/add",function (req, res) {
-    const text = req.body.text;
-    const cook = req.body.cook;
-    const user = req.body.user;
-    const rating = req.body.rating;
-  
+router.post("/comment/add", function (req, res) {
+  const text = req.body.text;
+  const cook = req.body.cook;
+  const user = req.body.user;
+  const rating = req.body.rating;
 
-    const pdt = new Comment({
-      text: text,
-      cook:cook,
-      user:user,
-      rating:rating,
-    
+  const pdt = new Comment({
+    text: text,
+    cook: cook,
+    user: user,
+    rating: rating,
+  });
+
+  pdt
+    .save()
+    .then(function () {
+      res.json({ msg: "Comment added!!" });
+    })
+
+    .catch(function () {
+      res.json({ msg: "Something went wrong!" });
     });
-
-    pdt
-      .save()
-      .then(function () {
-        res.json({ msg: "Comment added!!" });
-      })
-
-      .catch(function () {
-        res.json({ msg: "Something went wrong!" });
-      });
-  }
-);
-
-
+});
 
 router.get("/comment/details/:id", function (req, res) {
   const text = req.body.text;
-  
-    
+
   //make userid in product model
 
-  Comment.find({cook:req.params.id}).populate('user')
+  Comment.find({ cook: req.params.id })
+    .populate("user")
     .then(function (result) {
       res.json(result);
       console.log(result);
@@ -49,6 +44,26 @@ router.get("/comment/details/:id", function (req, res) {
     });
 });
 
+
+router.put("/comment/like", function (req, res) {
+  const id = req.body.cid;
+
+  Comment.findByIdAndUpdate(
+    id ,{
+
+      $push: { like: req.body.userid },
+
+  },
+
+  { new: true },(err,doc)=>{
+      console.log(req.body.userid)
+      res.json({success:true})
+
+    
+    })
+  })
+
+  
 
 
 module.exports = router;
